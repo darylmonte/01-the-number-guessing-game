4bc2360d733ad5d20e8b2329e4fc99068299b198
Daryl Monte <darylmonte@gmail.com>
2022-02-10 22:11:02 -0500

    for submission

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# 01-the-number-guessing-game
+# The Number Guessing Game
+
+The computer is going to randomly select an integer from 1 to 100. The player will keep guessing until the player finds the computer's number. The computer will tell each time if the player's guess was too high or low. After the player has successfully find the secret number, it will show the number of attempts made.
+
+The computer will then show the score to beat (least number of guesses) if the player wants to play another round otherwise it will say goodbye and terminates the game.
  
--- guessing_game.py
+++ guessing_game.py
@@ -2,36 +2,63 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
+import random, time
 
-import random
+print(
+'''
+=======================================
+|                                     |
+|       GUESS THE SECRET NUMBER       |
+|                                     |
+=======================================
+'''
+    )
 
+scores = [] # stores the score of each game
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
 
+    lower_limit = 1
+    upper_limit = 100
+    ANSWER = random.randint(lower_limit, upper_limit)
 
+    print(f"I'm thinking of a number between {lower_limit} and {upper_limit}, can you guess what it is?")
+    time.sleep(1)
+
+    try:
+        guess = int(input(f"Enter a number between {lower_limit} and {upper_limit}: "))
+        attempts = 1
+
+        while guess != ANSWER:
+            if guess > upper_limit:
+                print(f"Try again. My secret number is only between 1 and 100.\n")
+            elif guess > ANSWER:
+                print(f"Your guess is too high.\n")
+            elif guess < ANSWER:
+                print(f"Your guess is too low.\n")
+
+            guess = int(input(f"Enter a number between {lower_limit} and {upper_limit}: "))
+            attempts += 1
+    except ValueError:
+        print("\nxXX-- Oh no! That's not a valid number. Let's start again. --XXx\n")
+        start_game()
+    else:
+        print(f"\nFantastic, you got it!!!\nIt took you {attempts} tries to guess the secret number.\n")
+        scores.append(attempts)
 
 # Kick off the program by calling the start_game function.
 start_game()
+
+play_again = input("Would you like to play again? [y]es/[n]o: ")
+while play_again.lower() == "y" or play_again.lower() == "yes":
+    # Reference: https://www.geeksforgeeks.org/python-program-to-find-smallest-number-in-a-list/
+    if min(scores) == 1:
+        print("\nSomeone got lucky and guessed the number in a single attempt. Try your luck...\n")
+    else: 
+        print(f"\n===> The score to beat is {min(scores)} attempts! <===\n")
+    start_game()
+    play_again = input("Would you like to play again? [y]es/[n]o: ")
+
+
+print("Thank you for playing! Ciao!")
