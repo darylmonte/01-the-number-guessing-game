9639d1191ad0ad3de685bdb463bfe0768590ceba
Daryl Monte <darylmonte@gmail.com>
2022-02-11 22:39:21 -0500

    added game stats

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # The Number Guessing Game
 
-The computer is going to randomly select an integer from 1 to 100. The player will keep guessing until the player finds the computer's number. The computer will tell each time if the player's guess was too high or low. After the player has successfully find the secret number, it will show the number of attempts made.
+The computer is going to randomly select an integer from 1 to 100. The player will keep guessing until the player finds the computer's number. The computer will tell each time if the player's guess was too high or low. After the player has successfully find the secret number, it will show the number of attempts made and the round's game stats (mean, median and mode of the attempted numbers).
 
-The computer will then show the score to beat (least number of guesses) if the player wants to play another round otherwise it will say goodbye and terminates the game.
+The computer will then show the score to beat (least number of guesses), otherwise it will say goodbye and terminates the game.
  
--- guessing_game.py
+++ guessing_game.py
@@ -3,59 +3,74 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 """
-import random, time
+import random
+import time
+from statistics import mean
+from statistics import median
+from statistics import mode
 
-print(
-'''
+print('''
 =======================================
 |                                     |
 |       GUESS THE SECRET NUMBER       |
 |                                     |
 =======================================
-'''
-    )
+''')
 
-scores = [] # stores the score of each game
+scores = []  # stores the score of each game
+
 
 def start_game():
 
     lower_limit = 1
     upper_limit = 100
     ANSWER = random.randint(lower_limit, upper_limit)
+    attempted_numbers = []
 
     print(f"I'm thinking of a number between {lower_limit} and {upper_limit}, can you guess what it is?")
-    time.sleep(1)
+    time.sleep(0.5)
 
     try:
         guess = int(input(f"Enter a number between {lower_limit} and {upper_limit}: "))
         attempts = 1
+        attempted_numbers.append(guess)
 
         while guess != ANSWER:
             if guess > upper_limit or guess < lower_limit:
-                print(f"Try again. My secret number is only between 1 and 100.\n")
+                print("Try again. My secret number is only between 1 and 100.\n")
             elif guess > ANSWER:
-                print(f"Your guess is too high.\n")
+                print("Your guess is too high.\n")
             elif guess < ANSWER:
-                print(f"Your guess is too low.\n")
+                print("Your guess is too low.\n")
 
             guess = int(input(f"Enter a number between {lower_limit} and {upper_limit}: "))
             attempts += 1
+            attempted_numbers.append(guess)
     except ValueError:
         print("\nxXX-- Oh no! That's not a valid number. Let's start again. --XXx\n")
         start_game()
     else:
-        print(f"\nFantastic, you got it!!!\nIt took you {attempts} tries to guess the secret number.\n")
+        print(f'''
+Fantastic, you got it!!!\nIt took you {attempts} tries to guess the secret number.
+-----------------
+ YOUR GAME STATS
+-----------------
+Mean: {mean(attempted_numbers)}
+Median: {median(attempted_numbers)}
+Mode: {mode(attempted_numbers)}
+        ''')
         scores.append(attempts)
+        time.sleep(2.5)
+
 
 # Kick off the program by calling the start_game function.
 start_game()
 
 play_again = input("Would you like to play again? [y]es/[n]o: ")
 while play_again.lower() == "y" or play_again.lower() == "yes":
-    # Reference: https://www.geeksforgeeks.org/python-program-to-find-smallest-number-in-a-list/
     if min(scores) == 1:
-        print("\nSomeone got lucky and guessed the number in a single attempt. Try your luck...\n")
-    else: 
+        print("\nSomebody guessed the number in a single attempt!!! Try beating that...\n")
+    else:
         print(f"\n===> The score to beat is {min(scores)} attempts! <===\n")
     start_game()
     play_again = input("Would you like to play again? [y]es/[n]o: ")
